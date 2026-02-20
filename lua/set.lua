e488786cff03e5429a67565e7a3c7b8a1a55256a
Guenael Roger <guenael.mail@gmail.com>
2026-02-20 15:17:58 +0100

    removed unsued paramters and lots of comments

--- lua/set.lua
+++ lua/set.lua
@@ -7,91 +7,48 @@
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
--- Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.smarttab = true
---vim.o.expandtab = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 -- vim.o.mouse = "a"
-
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
--- vim.schedule(function()
--- 		vim.o.clipboard = "unnamedplus"
--- end)
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Keep signcolumn on by default
-vim.o.signcolumn = "yes"
-
--- Decrease update time
--- vim.o.updatetime = 50
-
--- Decrease mapped sequence wait time
--- vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
---vim.o.splitright = true
---vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-options-guide`
---vim.o.list = true
---vim.opt.listchars = { tab = "| ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
--- vim.o.inccommand = "split"
-
--- Show which line your cursor is on
-vim.o.cursorline = true
-
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
+-- Add a confirmation before leaving unsaved file
 vim.o.confirm = true
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
+-- More helpers
+vim.o.undofile = true
+vim.o.signcolumn = "yes"
+vim.o.cursorline = true
+
+-- For highlight interaction
 vim.opt.hls = true
 vim.opt.incsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.opt.wrap = true -- soft wrap
-vim.opt.linebreak = true -- wrap at word boundaries
-vim.opt.breakindent = true -- keep indentation on wrapped lines
+-- For line above 80 characters
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
 
+-- For spelling mistakes
 vim.opt.spelllang = { "en_us", "fr" }
 vim.opt.spell = true
