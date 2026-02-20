f667fcc142e8da0cf7fb0f268fece0e7af1d4635
Guenael Roger <guenael.mail@gmail.com>
2026-02-20 14:10:47 +0100

    added lualine usage

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,10 +4,13 @@ return { -- You can easily change to a different colorscheme.
 	--
 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 	"catppuccin/nvim",
+	dependencies = {
+		{ "nvim-lualine/lualine.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
+	},
 	config = function()
 		require("catppuccin").setup({
 			name = "catppuccin",
-			priority = 999,
+			priority = 1000,
 			transparent_background = true,
 			float = {
 				transparent = true,
@@ -16,8 +19,7 @@ return { -- You can easily change to a different colorscheme.
 		})
 
 		-- Load the colorscheme here.
-		-- Like many other themes, this one has different styles, and you could load
-		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("catppuccin-macchiato")
+		require("lualine").setup()
 	end,
 }
